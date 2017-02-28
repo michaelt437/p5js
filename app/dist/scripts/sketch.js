990504278cf879ecf02849a26110a05310fc5ba2
michaelt437 <michaelt437@gmail.com>
2017-02-28 00:32:49 -0500

    mo trails

--- app/dist/scripts/sketch.js
+++ app/dist/scripts/sketch.js
@@ -1,7 +1,7 @@
-var count = 50;
+var count = 75;
 var org = [];
 function setup() {
-    createCanvas(1000,400);
+    createCanvas(windowWidth,500);
     for(var i = 0; i < count; i++){
         org[i] = new Organism();
     }
@@ -11,11 +11,11 @@ function draw() {
     // background(112,2,112);
     for(var i = 0; i < org.length; i++){
         org[i].move();
-        console.log(org[i].x, org[i].y);
-        if(org[i].y > (height + ((org[i].size)/2))){
-            console.log('greater than 100');
-            org[i].y = 0;
-            console.log(org[i].y, 'should be at top');
+        if(org[i].y > (height + org[i].radius)){
+            org[i].y = 0 - org[i].radius;
+        }
+        if(org[i].x > (width + org[i].radius)){
+            org[i].x = 0 - org[i].radius;
         }
         org[i].display();
     }
@@ -23,22 +23,26 @@ function draw() {
 }
 
 function Organism(){
-    this.size = random(15, 50);
+    this.size = random(15, 70);
+    this.radius = this.size/2;
     this.r = 255;
     this.g = 255;
     this.b = 255;
-    this.x = random(0, 1000);
-    this.y = random(0, 400);
+    this.changeRate = .3;
+    this.alpha = 0;
+    this.x = random(0, width);
+    this.y = random(0, height);
 
     this.move = function() {
         this.x += (noise(this.x*(random(-100,100)), this.y*(random(-100,100)), frameCount*.025) - .45) * 20;
         this.y += (noise(frameCount*.025, this.x*(random(-100,100)), this.y*(random(-100,100))) - .45) * 20;
-        this.g -= .3;
-        this.b -= .3;
+        this.b -= this.changeRate;
+        this.g -= this.changeRate;
 
     }
 
     this.display = function(){
+        stroke(0);
         fill(this.r,this.g,this.b);
         ellipse(this.x, this.y, this.size, this.size);
     };
